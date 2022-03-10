49e12d44ee8534447fa2531733c96b6470749fd3
Prerit Das <preritdas@gmail.com>
2022-03-09 18:56:46 -0800

    Added support for treble and bass clef.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from matplotlib import pyplot as plt
 import pandas as pd
-import sys
 import numpy as np
-from os import path
+
+import sys
+import os
 import shutil
+
+# Required: Treble or Bass Clef
+if 'treble' in sys.argv:
+    mode = 'Treble'
+elif 'bass' in sys.argv:
+    mode = 'Bass'
+else:
+    raise Exception("You must specify treble or bass.")
 
 # Initiation: possibly create new student or if not, get name
 if sys.argv[1] == 'create':
     student_name = sys.argv[2].title()
-    shutil.copy(src = 'Students/Copy.csv', dst = f'Students/{student_name}.csv')
+    shutil.copy(src = f'Students/Copy-{mode}.csv', dst = f'Students/{student_name}-{mode}.csv')
 else:
     try:
         student_name = sys.argv[1].title()
@@ -16,13 +25,13 @@ else:
         student_name = 'Sample'
 
 # Check is student is existing valid
-while not path.exists(f'Students/{student_name}.csv'):
+while not os.path.exists(f'Students/{student_name}-{mode}.csv'):
     student_name = input("That student's file doesn't exist. Enter a new one. >>> ")
 
 def load_data(student: str = student_name):
     """Loads a CSV of student's data and returns a DataFrame."""
     try:
-        return pd.read_csv(f'Students/{student}.csv', index_col = 0)
+        return pd.read_csv(f'Students/{student}-{mode}.csv', index_col = 0)
     except FileNotFoundError:
         print('File not found. Try again.')
         student = input("Enter the student's name. >>> ")
@@ -30,17 +39,17 @@ def load_data(student: str = student_name):
 
 def save_data(data: pd.DataFrame, student: str = student_name):
     """Takes student's name and their data and writes a file (or updates) CSV with their data."""
-    data.to_csv(f'Students/{student}.csv')
+    data.to_csv(f'Students/{student}-{mode}.csv')
 
 def save_plot(data: pd.DataFrame, student: str = student_name):
     """Takes a DataFrame and saves a student's data to Graphs folder."""
     fig, ax = plt.subplots()
     plt.style.use('fivethirtyeight')
-    ax.set_title(f'Note Reading Accuracy: {student}')
+    ax.set_title(f'{mode} Note Reading Accuracy: {student}')
     ax.set_xlabel('Note')
     ax.set_ylabel('Accuracy')
     ax.bar(data.index, data['Accuracy'])
-    fig.savefig(f'Graphs/{student}')
+    fig.savefig(f'Graphs/{student}-{mode}')
 
 def fix_nan(data: pd.DataFrame, note: str):
     data['Correct'][note] = 0
